c7d3f7a52a1923694360952e8da85f0c46a61f51
lhy8479 <847906234@qq.com>
2019-11-08 17:26:41 +0800

    insertsort

--- test.c
+++ test.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
-int main()
+#define N 9
+int main(void)
 {
-    printf("Hello World!\n");
-    printf("hahaha\n");
+    int a[N] = {6,5,8,7,9,4,3,1,2};
+    for(int i=1;i<N;i++)
+    {
+        int j, key=a[i];
+        for(j=i-1;j>=0;j--)
+        {
+            if(a[j]>key)
+            {
+                a[j+1]=a[j];
+            }else
+            {
+                break;
+            }
+        }
+        a[j+1]=key;
+    }
+    for(int i=0;i<N;i++)
+
+    printf("%d",a[i]);
+
     return 0;
 }
